295a591deb57c8bd777e5d80bdd17563185e927a
Kayton <kaytonfletcher@gmail.com>
2019-08-04 23:15:50 -0500

    Updates

---
+++ precache-manifest.2af672c37055fdcc5091b14bb4c3948e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeab101fb0278f86c765d94b9e57a174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e3580cbb03e839b567",
+    "url": "/static/css/main.c3efccdd.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "b7e3580cbb03e839b567",
+    "url": "/static/js/main.bba3704e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
