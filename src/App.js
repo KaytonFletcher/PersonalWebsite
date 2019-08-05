52e05a94da98550a2536fb00e79f0912e8284392
Kayton <kaytonfletcher@gmail.com>
2019-08-04 23:03:00 -0500

    Adding theming

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const lightTheme = {
+  "--color-solid": "black",
+  "--color-surface": "white",
+  "--color-primary": "teal"
+};
+const darkTheme = {
+  "--color-solid": "white",
+  "--color-surface": "black",
+  "--color-primary": "purple"
+};
+
+const App = () => {
+  const [currentTheme, setTheme] = React.useState("light");
+
+  React.useEffect(() => {
+    //changes theme by setting css vars from light to dark
+    const theme = (currentTheme === "light") ? lightTheme : darkTheme;
+    for (var key in theme) {
+      const value = theme[key];
+      document.documentElement.style.setProperty(key, value);
+    }
+  }, [currentTheme])
+
+  const onClick = () => {
+    const nextTheme = currentTheme === "light" ? "dark" : "light";
+    setTheme(nextTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h1>{currentTheme === "light" ? "Light theme" : "Dark theme"}</h1>
+      <button onClick={onClick}>Toggle theme</button>
     </div>
   );
 }
---
+++ src/Components/ScienceLab/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScienceLab = () => {
+    
+
+
+
+
+}
+
+export default ScienceLab;
